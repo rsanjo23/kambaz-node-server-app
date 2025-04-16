51ad1774db769c7d0f6b5ff250c3d329d9efaf1a
Rhinoguana <57597522+Rhinoguana@users.noreply.github.com>
2025-04-16 16:02:53 -0400

    Did 6.4.3.5

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -4,6 +4,12 @@ import * as enrollmentsDao from "../Enrollments/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
 
 export default function CourseRoutes(app) {
+    const findUsersForCourse = async (req, res) => {
+        const { cid } = req.params;
+        const users = await enrollmentsDao.findUsersForCourse(cid);
+        res.json(users);
+    };
+    app.get("/api/courses/:cid/users", findUsersForCourse);
     app.post("/api/courses/:courseId/assignments", (req, res) => {
         const { courseId } = req.params;
         const assignment = {
